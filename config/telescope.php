5a2c33324760657d296fd3206c0f50857b037765
jcleong <jcleong.edgetech@gmail.com>
2024-05-22 19:24:17 +0800

    Turned off the telescope inserting to db

--- config/telescope.php
+++ config/telescope.php
@@ -63,7 +63,7 @@ return [
     |
     */
 
-    'enabled' => env('TELESCOPE_ENABLED', true),
+    'enabled' => env('TELESCOPE_ENABLED', false),
 
     /*
     |--------------------------------------------------------------------------
